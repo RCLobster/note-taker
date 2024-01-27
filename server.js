968dc6ec7c98de83b88a708d359e08b684411c5a
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-01-27 12:18:08 -0800

    able to get and post data to db.json

--- server.js
+++ server.js
@@ -32,8 +32,60 @@ res.sendFile(path.join(__dirname, '/public/notes.html'))
 // GET route to return data in db.json
 app.get('/api/notes', (req, res) => {
     console.log(`${req.method} request received`);
-    //fs.readFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    let notes = require('./db/db.json');
     return res.json(notes);
+
+    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //     err
+    //         ? console.error(err)
+    //         : console.log(`Retrieved data`);
+    // });
+    
+});
+
+// POST route to add new notes into db.json
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method} request received`);
+    
+    // deconstruct the object to add
+    const {title, text} = req.body;
+    
+    // if that object contains a property for title AND text...
+    if(title && text) {
+        // then create a new object named newNote with the passed data
+        const newNote = {
+            title,
+            text
+        };
+
+        // read the current contents of db.json
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            err
+                ? console.error(err)
+                : console.log(`note titled ${newNote.title} has been written to JSON file`);
+        
+            // parse the data from db.json
+            const returnedData = JSON.parse(data);
+            // push the newly created note into db.json array
+            returnedData.push(newNote);
+            
+            // write a new db.json file with the new data
+            fs.writeFile('./db/db.json', JSON.stringify(returnedData, null, 4), (err) => 
+                err
+                    ? console.error(err)
+                    : console.log(`new note titled ${newNote.title} added to db.json`));
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+          };
+      
+          console.log(response);
+          res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
 });
 
 
